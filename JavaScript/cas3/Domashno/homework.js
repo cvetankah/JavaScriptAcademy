7153a39cdf654e5126465fd698c4125d38c739fa
cvetankahadjiev <cvetanka888@yahoo.com>
2022-05-10 17:14:04 +0200

    changed a file

--- JavaScript/cas3/Domashno/homework.js
+++ JavaScript/cas3/Domashno/homework.js
@@ -19,4 +19,24 @@ for (let i = 0; i < temp.length; i++) {
 console.log("Maximum is" + ' ' + high)
 console.log("Minimum is" + ' ' + low)
 
+// const temp = [12, 15, 14, 20, 22, 30]
+// let sum = 0
+// let high = temp[0]
+// let low = temp[0]
+
+// for (let index = 0; index < temp.length; index ++) {
+//     sum += temp[index]
+// }
+
+// console.log(sum / temp.length)
+
+
+// for (let i = 0; i < temp.length; i++) {
+//     high = i > temp[i]; 
+//     low = i < temp[i]
+// }
+
+// console.log("Maximum is" + ' ' + high)
+// console.log("Minimum is" + ' ' + low)
+
 
